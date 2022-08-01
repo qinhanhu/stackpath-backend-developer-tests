ff1b491c5802171585c876ecdff1622313bee656
QinhanHu <huqinhan1997@gmail.com>
2022-08-01 15:39:14 -0400

    unitest

--- unit-testing/pkg/fizzbuzz/fizzbuzz_test.go
+++ unit-testing/pkg/fizzbuzz/fizzbuzz_test.go
@@ -2,42 +2,58 @@ package fizzbuzz
 
 import (
 	"fmt"
+	"github.com/stretchr/testify/assert"
 	"testing"
 )
 
-type fizzBuzzTest struct {
+//type fizzBuzzTest struct {
+//	name                  string
+//	total, fizzAt, buzzAt int64
+//	expected              []string
+//	wantPanic             bool
+//}
+
+var fizzBuzzTests = []struct {
+	name                  string
 	total, fizzAt, buzzAt int64
-	expected []string
-}
-
-var fizzBuzzTests = []fizzBuzzTest{
-	fizzBuzzTest{0, 0, 0, []string{}},
-	fizzBuzzTest{0, 0, 0, []string{}},
-	fizzBuzzTest{0, 0, 0, []string{}},
-	fizzBuzzTest{0, 0, 0, []string{}},
-	fizzBuzzTest{0, 0, 0, []string{}},
-}
-
-func stringSlicesEqual(a, b []string) bool {
-	if len(a) != len(b){
-		return false
-	}
-	for i, v := range a{
-		if v != b[i] {
-			return false
-		}
-	}
-	return true
+	expected              []string
+	wantPanic             bool
+}{
+	// Write test cases here
+	{"test empty", 0, 0, 0, []string{}, false},
+	{"test panic: divided by zero, fizzAt = 0", 1, 0, 1, []string{}, true},
+	{"test panic: divided by zero, buzzAt = 0", 1, 1, 0, []string{}, true},
+	{"test result", 15, 3, 5, []string{"1", "2", "Fizz", "4", "Buzz", "Fizz", "7", "8", "Fizz", "Buzz", "11", "Fizz", "13", "14", "FizzBuzz"}, false},
 }
 
 func TestFizzBuzz(t *testing.T) {
-	//t.Skip("TODO: Add tests")
-	for _, test := range fizzBuzzTests{
-		if result := FizzBuzz(test.total, test.fizzAt, test.buzzAt); !stringSlicesEqual(result, test.expected){
-			t.Errorf("Output %q not equal to expected %q\n", result, test.expected)
-		}
+	for _, test := range fizzBuzzTests {
+		t.Run(test.name, func(t *testing.T) {
+			defer func() {
+				r := recover()
+				if (r != nil) != test.wantPanic {
+					t.Errorf("unexpected panic: recover = %v, case = %v", r, test)
+				}
+			}()
+			//if result := FizzBuzz(test.total, test.fizzAt, test.buzzAt); !stringSlicesEqual(result, test.expected){
+			//	t.Errorf("Output %q not equal to expected %q\n", result, test.expected)
+			//}
+			result := FizzBuzz(test.total, test.fizzAt, test.buzzAt)
+			assert.Equal(t, result, test.expected)
+		})
 	}
 }
+
+//func integerDivideByZeroPanic(t *testing.T) {
+//	defer func() {
+//		if r := recover(); r == nil {
+//			t.Errorf("The func did not panic as expected")
+//		}
+//	}()
+//	a := 5
+//	b := 0
+//	_ = a / b
+//}
 
 func BenchmarkFizzBuzz(b *testing.B) {
 	for i := 0; i < b.N; i++ {
